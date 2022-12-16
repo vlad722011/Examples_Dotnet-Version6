70e0cda144e7635509fb20d4f675f278881e322e
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-16 11:26:20 +0700

    Added Example007_Base

---
+++ Example007_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 6;
+int c = 2;
+int d = 23;
+int e = 4;
+
+int max =a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+
+Console.WriteLine("max = " + max);

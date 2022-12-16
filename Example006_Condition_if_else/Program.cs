fcfd7f05be8abcbde7c11cc81c69a2cda9eb0197
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-16 11:23:55 +0700

    Added Example006_Condition_if_else

---
+++ Example006_Condition_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+
+else
+
+{
+    Console.WriteLine($"Привет, {username}!");
+}

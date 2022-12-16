d962ecfa3e20035cbefa74c6c52efce26f1318f0
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-16 11:16:42 +0700

    Added Example005_Random

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10); // случайное число из диапозона [1 - 9] включительно.
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);

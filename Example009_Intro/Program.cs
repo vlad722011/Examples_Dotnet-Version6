77ee6bd95906ebfae8b2e9ff2ab56c3c9384a0fa
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-18 23:32:51 +0700

    added example009 and example010

---
+++ Example009_Intro/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 398;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33432;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33;
+int max = a1;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// max = Max(max1, max2, max3);
+
+max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
